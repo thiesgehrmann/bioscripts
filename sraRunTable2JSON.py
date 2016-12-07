ec9a69bec2184a8630eb04e2b5b24a11849a6907
thiesgehrmann <thiesgehrmann@gmail.com>
2016-12-07 11:52:27 +0100

    Updated 2JSON

--- sraRunTable2JSON.py
+++ sraRunTable2JSON.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python2
 
 import os;
-import ibidas
+from ibidas import *
 import json
 
 ###############################################################################
 
 def srt2dict(SRT):
-  SRTf = SRT.Get(_.bioproject_s, _.biosample_s, _.experiment_s, _.run_s)
+  SRTf = SRT.Get(_.bioproject_s, _.biosample_s, _.sample_name_s, _.sra_sample_s, _.experiment_s, _.run_s)
   SRT  = SRT.Get(*(SRTf.Names + list(set(SRT.Names) - set(SRTf.Names))))
   biop = SRT.GroupBy(_.bioproject_s)
   bios = biop.GroupBy(_.biosample_s)
@@ -16,38 +16,50 @@ def srt2dict(SRT):
   JSON_S = ""
   D = {}
 
-  D["bioprojects"] = SRT.bioproject_s.Unique()().tolist()
-  D["biosamples"]  = SRT.biosample_s.Unique()().tolist()
-  D["experiments"] = SRT.experiment_s.Unique()().tolist()
-  D["runs"]        = SRT.run_s.Unique()().tolist()
+  D["origin"]       = "SRA Run Table"
+  D["depth"]        = 4
+  D["depth_labels"] = [ "project", "sample", "experiment", "measurement" ]
 
-  bioprojects = {}
-  for bioproject_level in zip(*exp()):
-    bioproject = {}
-    bioproject["id"] = bioproject_level[0]
-    biosamples = []
-    for biosample_level in zip(*bioproject_level[1:]):
-      biosample = {}
-      biosample["id"] = biosample_level[0]
+  D["project_list"]      = SRT.bioproject_s.Unique()().tolist()
+  D["sample_list"]       = SRT.biosample_s.Unique()().tolist()
+  D["experiment_list"]   = SRT.experiment_s.Unique()().tolist()
+  D["measurement_list"]  = SRT.run_s.Unique()().tolist()
+
+  projects = []
+  for project_level in zip(*exp()):
+    project = {}
+    project["id"]   = project_level[0]
+    project["type"] = "project"
+    samples = []
+    for sample_level in zip(*project_level[1:]):
+      sample = {}
+      sample["id"]   = sample_level[0]
+      sample["name"] = sample_level[1][0][0]
+      sample["sra_sample_s"] = sample_level[2][0][0]
+      sample["type"] = "sample"
       experiments = []
-      for experiment_level in zip(*biosample_level[1:]):
+      for experiment_level in zip(*sample_level[3:]):
         experiment = {}
-        experiment["id"] = experiment_level[0]
-        runs = []
-        for run_level in zip(*experiment_level[1:]):
-          run = dict(zip(SRT.Names[3:], run_level))
-          runs.append(run)
+        experiment["id"]   = experiment_level[0]
+        experiment["type"] = "experiment"
+        measurements = []
+        for measurement_level in zip(*experiment_level[1:]):
+          measurement_id = measurement_level[0]
+          measurement = dict(zip(SRT.Names[-len(measurement_level[1:]):], measurement_level[1:]))
+          measurement["id"]   = measurement_id
+          measurement["type"] = "measurement"
+          measurements.append(measurement)
         #efor
-        experiment["runs"] = runs
-        experiments.append(runs)
+        experiment["measurements"] = measurements
+        experiments.append(experiment)
       #efor
-      biosample["experiments"] = experiments
-      biosamples.append(biosample)
+      sample["experiments"] = experiments
+      samples.append(sample)
     #efor
-    bioproject["biosamples"] = biosamples
-    bioprojects[bioproject_level[0]] = bioproject
+    project["samples"] = samples
+    projects.append(project)
   #efor
-  D["data"] = bioprojects
+  D["data"] = projects
 
   return D
 #edef
@@ -60,7 +72,7 @@ sraRunTable = os.sys.argv[1];
 
 SRT  = Read(sraRunTable)
 DICT = srt2dict(SRT)
-JSON = json.dumps(DICT)
+JSON = json.dumps(DICT, indent=3, sort_keys=True)
 
 print JSON
 
